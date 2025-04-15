a7cd03a92c3c51239039de968ab727ebef45e59c
Jamcat42 <jamcatjunk@outlook.com>
2025-04-15 10:34:20 -0400

    Update NavControl.cs

--- The Wyrm/Assets/NavControl.cs
+++ The Wyrm/Assets/NavControl.cs
@@ -6,24 +6,64 @@ using UnityEngine.AI;
 public class NavControl : MonoBehaviour
 {
 
-    //[SerializeField] Transform goal;
+    [SerializeField] Transform goal;
     Vector3 end;
     NavMeshAgent agent;
-    GameObject player;
+
+    NavMeshPath path;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
+        goal = GameObject.Find("Player").GetComponent<Transform>();
         agent = GetComponent<NavMeshAgent>();
-        end = player.transform.position;
+        end = agent.destination;
+        agent.speed = 3.5f;
+        path = new NavMeshPath();
     }
 
     // Update is called once per frame
     void Update()
     {
-        end = player.transform.position;
-        agent.destination = end;
-        Debug.Log(agent.remainingDistance);
+        end = goal.position;
+        //
+        if (agent.CalculatePath(end, path) && path.status == NavMeshPathStatus.PathComplete) //Look into why it cant traverse links anymore
+        {
+            agent.destination = end;
+            //agent.SetPath(path);
+        }
+        else
+        {
+            agent.destination = agent.transform.position;
+        }
+        //Debug.Log(agent.remainingDistance);
+        if (agent.isOnOffMeshLink)
+        {
+            agent.speed = 6.5f / 2f;
+        }
+        else
+        {
+            agent.speed = 6.5f;
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("trigger");
+        if (other.tag == "Slow")
+        {
+            agent.speed = 7f * 0.5f;
+            Debug.Log("slow zone");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Slow")
+        {
+            agent.speed = 7f;
+        }
     }
 }
