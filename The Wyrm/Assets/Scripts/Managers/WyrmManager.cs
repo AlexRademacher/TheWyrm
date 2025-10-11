b74b4e67466abca890b084a2a7e9ba2c863e3bf2
Fl4m3l1ng <rademaat@miamioh.edu>
2025-10-10 20:01:48 -0400

    Working Wyrm in new arena

--- The Wyrm/Assets/Scripts/Managers/WyrmManager.cs
+++ The Wyrm/Assets/Scripts/Managers/WyrmManager.cs
@@ -1,24 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 public class WyrmManager : MonoBehaviour
 {
+    private GameManager GM;
+
+    private Transform player;
+
+    private NavMeshAgent agent;
+    private NavMeshPath path;
+
+    private bool inArena;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GM = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        if (SceneManager.GetActiveScene().name.Contains("Arena"))
+        {
+            player = GameObject.Find("Player").GetComponent<Transform>();
+
+
+        }
+
+        path = new NavMeshPath();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Cursor.visible && !GM.GetLoadingState())
+        {
+            if (inArena)
+            {
+                if (agent.CalculatePath(player.position, path) && path.status == NavMeshPathStatus.PathComplete)
+                {
+                    agent.destination = player.position;
+                }
+                else
+                {
+                    if (agent.remainingDistance < 0.5f)
+                    {
+                        GotoNextPoint();
+                    }
+                }
+            }
+        }
     }
 
     public void SetPlayerPosition(Transform playerTransform)
     {
+        player = playerTransform;
+    }
 
+    private void GotoNextPoint()
+    {
+        
     }
 
     public IEnumerator StartCountdownToLeave(int time, WyrmSpawnManager WSM)
