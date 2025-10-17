a7088059881a59b33f717cfb1eae92f748c84312
Jamcat42 <jamcatjunk@outlook.com>
2025-10-17 10:53:45 -0400

    if dialog doesnt exist does nothing

--- The Wyrm/Assets/Scripts/Player/PlayerInteraction.cs
+++ The Wyrm/Assets/Scripts/Player/PlayerInteraction.cs
@@ -97,12 +97,16 @@ public class PlayerInteraction : MonoBehaviour
                         if (!hitInfo.transform.GetComponent<NPCManager>().canNeverSpeak)
                         {
                             Dialog dialog = hitInfo.transform.GetComponent<Dialog>();
-                            if (dialog != null)
-                                Debug.Log("found Dialog");
                             if (hitInfo.transform.gameObject.GetComponent<NPCManager>().npcID == 8)
                                 lastNpcId = 8;
+                            if (dialog != null)
+                            {
+                                Debug.Log("found Dialog");
+                                StartCoroutine(DialogueManager.Instance.ShowDialog(dialog));
+                            }
+                            
                            
-                            StartCoroutine(DialogueManager.Instance.ShowDialog(dialog));
+                            
                         }
                         else
                             hitInfo.transform.GetComponent<NPCManager>().SetInTalkingState(false);
