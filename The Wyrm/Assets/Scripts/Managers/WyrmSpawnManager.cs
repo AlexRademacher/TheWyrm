cca86a65b42026610672eaf4923d958c42acfdf2
Fl4m3l1ng <rademaat@miamioh.edu>
2025-11-05 15:35:24 -0500

    fix so wyrm doesn't always spawn in village

--- The Wyrm/Assets/Scripts/Managers/WyrmSpawnManager.cs
+++ The Wyrm/Assets/Scripts/Managers/WyrmSpawnManager.cs
@@ -54,7 +54,7 @@ public class WyrmSpawnManager : MonoBehaviour
                 Debug.Log("spawning wyrm");
                 StartCoroutine(SpawnCountdown(1));
             }
-            else if (Random.Range(5, 7) > 4)
+            else if (Random.Range(1, 7) > 4)
             {
                 StartCoroutine(SpawnCountdown(waitTime));
             }
