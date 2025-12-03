ed76dae1b1ffdc49e8605bfb8573b907d7155bde
Jamcat42 <jamcatjunk@outlook.com>
2025-12-02 21:53:05 -0500

    fade testing

--- The Wyrm/Assets/FadeUI.cs
+++ The Wyrm/Assets/FadeUI.cs
@@ -46,7 +46,7 @@ public class FadeUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*Debug.Log(currentColor.a);
+        Debug.Log(currentColor.a);
         if (currentColor.a >= 10/255f && hiding == true) //if alpha is greater than ten decrease alpha
             currentColor = new Color(currentColor.r, currentColor.g, currentColor.b, currentColor.a - 10 / 255f); //decreases alpha over time
         else if (currentColor.a == 1f) // if alpha is at max allow alpha to go down
@@ -56,7 +56,7 @@ public class FadeUI : MonoBehaviour
             hiding = false;
             currentColor = new Color(currentColor.r, currentColor.g, currentColor.b, currentColor.a + 10 / 255f); //increases alpha over time
         }
-        GetComponent<Image>().color = currentColor;*/
+        GetComponent<Image>().color = currentColor;
     }
 
     public bool FadeIn(Color color)
