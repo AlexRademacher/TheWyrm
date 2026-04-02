25e9011462c6b280435f75086bfd9029dd4227c1
Jamcat42 <jamcatjunk@outlook.com>
2026-04-01 22:49:09 -0400

    "Fix"

--- The Wyrm/Assets/Scripts/NewDialogue/TriggerDialog.cs
+++ The Wyrm/Assets/Scripts/NewDialogue/TriggerDialog.cs
@@ -42,9 +42,9 @@ public class NewBehaviourScript : MonoBehaviour
             triggered = true;
             thisDialog = GetComponent<Dialog>();
 
-            Debug.Log(other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x);
+            //Debug.Log(other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x);
 
-            if (other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x >= -0.17 && other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x < 0)
+            /*if (other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x >= -0.17 && other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x < 0)
             {
                 Debug.Log("0 - 20");
                 CameraX = 20;
@@ -64,18 +64,38 @@ public class NewBehaviourScript : MonoBehaviour
                 Debug.Log("40 - 50");
                 CameraX = 50;
             }
+            else if (other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x >= -0.49 && other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x < -0.45)
+            {
+                Debug.Log("50 - 60");
+                CameraX = 60;
+            }
+            else if (other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x >= -0.56 && other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x < -0.49)
+            {
+                Debug.Log("60 - 70");
+                CameraX = 70;
+            }
+            else if (other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x >= -0.63 && other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x < -0.56)
+            {
+                Debug.Log("70 - 80");
+                CameraX = 80;
+            }
+            else if (other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x >= -0.7 && other.gameObject.transform.GetChild(0).GetChild(0).transform.rotation.x < -0.63)
+            {
+                Debug.Log("80 - 90");
+                CameraX = 90;
+            }
             else 
             {
                 Debug.Log("Else");
                 CameraX = 0;
-            }
+            }*/
 
-                other.gameObject.transform.LookAt(this.transform.position);
+            other.gameObject.transform.LookAt(this.transform.position);
 
             Debug.Log(other.gameObject.transform.GetChild(0).GetChild(0).name);
             //Add if statements to change amount of vertical rotation based on starting rotation (20)
 
-            other.gameObject.transform.eulerAngles = new Vector3(CameraX, other.transform.rotation.eulerAngles.y, other.transform.rotation.eulerAngles.z);
+            //other.gameObject.transform.eulerAngles = new Vector3(CameraX, other.transform.rotation.eulerAngles.y, other.transform.rotation.eulerAngles.z);
 
             //CV.TriggerCamUpdate(this.transform);
             StartCoroutine(DialogueManager.Instance.ShowDialog(thisDialog, this.gameObject));
